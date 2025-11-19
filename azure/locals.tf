ab815a32b331592d861f48eede406b309d0bb6d4
elyvely <elyvelynne@gmail.com>
2025-11-19 16:43:11 -0500

    temp: change restriction

--- azure/locals.tf
+++ azure/locals.tf
@@ -1,7 +1,6 @@
 locals {
   workspace_suffix = split("-", terraform.workspace)[length(split("-", terraform.workspace)) - 1]
 
-  # Define distinct IP address ranges per workspace to avoid overlap
   vnet_cidrs = {
     dev     = "10.2.0.0/16"
     staging = "10.1.0.0/16"
