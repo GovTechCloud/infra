1d9aac625da5b13506cbac0e40adb70272aa72fd
gelylyne <gladys.chy5@gmail.com>
2025-12-08 15:57:22 -0500

    fix: format

--- azure/azurerm_mysql.tf
+++ azure/azurerm_mysql.tf
@@ -1,18 +1,18 @@
 resource "azurerm_mysql_flexible_server" "flexible_mysql" {
-  name                   = "${terraform.workspace}-mysql-server"
-  resource_group_name    = azurerm_resource_group.main.name
-  location               = azurerm_resource_group.main.location
-  version                = "8.0.21"
-  administrator_login    = "adminuser"
-  administrator_password = var.mysql_admin_password
-  backup_retention_days  = 7 #1to 35 days  
-  geo_redundant_backup_enabled   = false  
-  sku_name               = "B_Standard_B1s"
-  public_network_access = "Enabled"
+  name                         = "${terraform.workspace}-mysql-server"
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = azurerm_resource_group.main.location
+  version                      = "8.0.21"
+  administrator_login          = "adminuser"
+  administrator_password       = var.mysql_admin_password
+  backup_retention_days        = 7 #1to 35 days  
+  geo_redundant_backup_enabled = false
+  sku_name                     = "B_Standard_B1s"
+  public_network_access        = "Enabled"
   storage {
-    auto_grow_enabled    = true
-    iops                 = 640
-    size_gb              = 20
+    auto_grow_enabled = true
+    iops              = 640
+    size_gb           = 20
   }
 }
 
