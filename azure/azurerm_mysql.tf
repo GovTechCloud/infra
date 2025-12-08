d716137dcfd58bfc8b4294b77fd3e294f273c341
gelylyne <gladys.chy5@gmail.com>
2025-12-08 15:53:11 -0500

    feaT: add db

---
+++ azure/azurerm_mysql.tf
@@ -0,0 +1,43 @@
+resource "azurerm_mysql_flexible_server" "flexible_mysql" {
+  name                   = "${terraform.workspace}-mysql-server"
+  resource_group_name    = azurerm_resource_group.main.name
+  location               = azurerm_resource_group.main.location
+  version                = "8.0.21"
+  administrator_login    = "adminuser"
+  administrator_password = var.mysql_admin_password
+  backup_retention_days  = 7 #1to 35 days  
+  geo_redundant_backup_enabled   = false  
+  sku_name               = "B_Standard_B1s"
+  public_network_access = "Enabled"
+  storage {
+    auto_grow_enabled    = true
+    iops                 = 640
+    size_gb              = 20
+  }
+}
+
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_public_access" {
+  name                = "AllowPublicAccess"
+  resource_group_name = azurerm_resource_group.main.name
+  server_name         = azurerm_mysql_flexible_server.flexible_mysql.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
+
+# resource "null_resource" "db_populate" {
+#   depends_on = [azurerm_mysql_flexible_server.mysql]
+
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       mysql -h ${azurerm_mysql_flexible_server.mysql.fqdn} \
+#             -P 3306 \
+#             -u adminuser \
+#             -p${var.mysql_admin_password} \
+#             -e "CREATE DATABASE IF NOT EXISTS mydb;"
+#       mysql -h ${azurerm_mysql_flexible_server.mysql.fqdn} \
+#             -P 3306 \
+#             -u adminuser \
+#             -p${var.mysql_admin_password} mydb < ./data/backup.sql
+#     EOT
+#   }
+# }
