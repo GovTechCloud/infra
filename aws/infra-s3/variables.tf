028201d3a4b3c17fb4c32a2e1729c6645ebf9d06
damariscall <damariscalleroz@gmail.com>
2025-12-10 15:42:03 -0600

    Infra de S3

---
+++ aws/infra-s3/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Nombre único del bucket S3 para hosting"
+  type        = string
+}
